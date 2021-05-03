a0ba43949b273065d51ae9b9d10e35203e1e48f9
humanscape-june <june.333@humanscape.io>
2021-05-03 14:44:35 +0900

    feat: add event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Rooms(models.Model):
+    name = models.CharField(max_length=15)
+
+class Events(models.Model):
+    room = models.ForeignKey(Rooms)
+    summary = models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+class Users(models.Model):
+    username = models.CharField(max_length=20)
+
+class EventUserMappings(models.Model):
+    event = models.ForeignKey(Events)
+    user = models.ForeignKey(Users)
