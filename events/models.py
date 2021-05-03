7905841772192869bc83909b42a83738f3b43654
humanscape-june <83257939+humanscape-june@users.noreply.github.com>
2021-05-03 14:46:05 +0900

    Revert "feat: add event model"

--- events/models.py
+++ events/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-class Rooms(models.Model):
-    name = models.CharField(max_length=15)
-
-class Events(models.Model):
-    room = models.ForeignKey(Rooms)
-    summary = models.CharField(max_length=200)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-
-class Users(models.Model):
-    username = models.CharField(max_length=20)
-
-class EventUserMappings(models.Model):
-    event = models.ForeignKey(Events)
-    user = models.ForeignKey(Users)
+# Create your models here.
