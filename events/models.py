a1dabef04c56a1afcde57de7735ab7fe950faa4a
humanscape-june <june.333@humanscape.io>
2021-05-03 14:51:19 +0900

    fix: event models 외래키 옵션 추가

--- events/models.py
+++ events/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 class Rooms(models.Model):
     name = models.CharField(max_length=15)
 
 class Events(models.Model):
-    room = models.ForeignKey(Rooms)
+    room = models.ForeignKey(Rooms, on_delete=models.DO_NOTHING)
     summary = models.CharField(max_length=200)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -13,5 +14,5 @@ class Users(models.Model):
     username = models.CharField(max_length=20)
 
 class EventUserMappings(models.Model):
-    event = models.ForeignKey(Events)
-    user = models.ForeignKey(Users)
+    event = models.ForeignKey(Events, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=CASCADE)
