ee5795579e26ea7d0a3201fefeb0d93aa95d061a
humanscape-june <june.333@humanscape.io>
2021-05-03 22:42:49 +0900

    feat: add event update view

--- reservationroom/urls.py
+++ reservationroom/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from batch_tasks import views as batch_task_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('update/', batch_task_view.get, name='update')
 ]
