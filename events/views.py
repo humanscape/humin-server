09ea1eb4a3aa7057c1b60eebd458c58077da55cd
humanscape-june <83257939+humanscape-june@users.noreply.github.com>
2021-05-03 16:21:36 +0900

    Revert "feat: add view, serializers"

--- events/views.py
+++ events/views.py
@@ -1,9 +1,3 @@
-from events.serializers import EventSerializer, RoomSerializer
-from events.models import Rooms
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import mixins
 
-class EventViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    queryset = Rooms.objects
-    serializer_class = RoomSerializer
+# Create your views here.
