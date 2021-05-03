8fbb56771e353215186f48ba32c1afab36eff631
humanscape-june <june.333@humanscape.io>
2021-05-03 17:19:26 +0900

    feat: add calender_id(google api id)

--- events/models.py
+++ events/models.py
@@ -3,6 +3,7 @@ from django.db.models.deletion import CASCADE
 
 class Rooms(models.Model):
     name = models.CharField(max_length=15)
+    calendar_id = models.CharField(max_length=100, null=True)
 
 class Events(models.Model):
     room = models.ForeignKey(Rooms, on_delete=models.DO_NOTHING)
