01818da75c0db74e45c8bb214cc5aa43493af80a
humanscape-june <june.333@humanscape.io>
2021-05-03 23:07:04 +0900

    feat: add events urls

--- events/views.py
+++ events/views.py
@@ -1,9 +1,18 @@
+from events import serializers
 from events.serializers import EventSerializer, RoomSerializer
 from events.models import Rooms
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import mixins
 
-class EventViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    queryset = Rooms.objects
-    serializer_class = RoomSerializer
+serializer = RoomSerializer
+query_set = Rooms.objects
+
+def list(request):
+    pass
+
+def retrieve(request, room_name):
+    pass
+
+def retrieve_first(request, room_name):
+    pass
