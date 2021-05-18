79c62f4d326ac7a21053cebeb950c628b6abf791
humanscape-june <june.333@humanscape.io>
2021-05-18 10:36:55 +0900

    feat: user email, name 분리

--- users/models.py
+++ users/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Users(models.Model):
     email = models.CharField(max_length=100)
+    name = models.CharField(max_length=20, null=True)
     # human = 1, mommy = 2
     organization = models.IntegerField(default=0)
